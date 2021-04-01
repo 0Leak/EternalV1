a53db4dcb778d69700b4e406d9527175ff9220ae
0Leak <81780685+0Leak@users.noreply.github.com>
2021-04-01 15:21:54 -0700

    Add files via upload

---
+++ Eternal/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Eternal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Eternal")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("10cff05d-f4f9-4a92-b4e2-8b3e9bc0d47b")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
